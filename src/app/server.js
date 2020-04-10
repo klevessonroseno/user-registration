2c2b65a05ae32ae1f9081b58a5a8798683766317
klevessonroseno <klevessonroseno42@gmail.com>
2020-04-10 12:00:02 -0300

    Criando middleware para verificar se um usuário existe dentro do array de usuários

--- src/app/server.js
+++ src/app/server.js
@@ -23,9 +23,20 @@ function checkIfNameAndAgeExists(req, res, next){
     return next();
 }
 
+function checkIfUserExistsInArray(req, res, next){
+    const { id } = req.params;
+    const user = users[id];
+
+    if(!user){
+        return res.status(404).json({ message: 'User not found'});
+    }
+
+    return next();
+}
+
 server.get('/users', (req, res) => res.json(users));
 
-server.get('/users/:id', (req, res) => {
+server.get('/users/:id', checkIfUserExistsInArray, (req, res) => {
     const { id } = req.params;
     return res.json(users[id]);
 });
@@ -36,7 +47,7 @@ server.post('/users', checkIfNameAndAgeExists, (req, res) => {
     return res.json(users);
 });
 
-server.put('/users/:id', checkIfNameAndAgeExists, (req, res) => {
+server.put('/users/:id', checkIfNameAndAgeExists, checkIfUserExistsInArray, (req, res) => {
     const { id } = req.params;
     const { name, age } = req.body;
     
@@ -45,7 +56,7 @@ server.put('/users/:id', checkIfNameAndAgeExists, (req, res) => {
     return res.status(200).json({ message: 'User updated'});
 });
 
-server.delete('/users/:id', (req, res) => {
+server.delete('/users/:id', checkIfUserExistsInArray, (req, res) => {
     const { id } = req.params;
     users.splice(id, 1);
     return res.status(200).json('User deleted');
