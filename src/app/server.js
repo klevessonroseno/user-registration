3430ad542b09393a855a367017c22126915c7c69
klevessonroseno <klevessonroseno42@gmail.com>
2020-04-10 10:59:11 -0300

    Primeira rota para listar todos os usuários

--- src/app/server.js
+++ src/app/server.js
@@ -5,4 +5,6 @@ const users = require('./models/Users');
 
 server.use(express.json());
 
+server.get('/users', (req, res) => res.json(users));
+
 server.listen(port, () => console.log(`Server at port ${port}`));
